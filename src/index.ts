d13cdf0128eb25e77c080d64b59e22b0ac803320
zufichris <zufichris@gmail.com>
2025-01-21 18:47:46 +0400

    Updated utils

--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,25 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 import router from "./http/routes/route_v1";
 import { Start } from "./http/server/httpServer";
 import { errorHandler, notFound } from "./http/middleware/error";
-import { StatusCodes } from "./global/enums";
-import { IUser } from "./data/entities/user";
-import { authControllers } from "./http/controllers/auth";
 import { DB } from "./config/dbConfig";
 import { env } from "./config/env";
 import { loggerMiddleware } from "./http/middleware/logger";
+
 new DB(env.mongo_uri).connect();
+
 const app = express();
-app.use(express.json({}));
+app.use(cors())
+app.use(express.json({}))
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    })
 );
 app.use(cookieParser());
 app.use(loggerMiddleware)
-app.get("/", async (req, res) => {
-  const user = await authControllers.verifyToken(req);
-  res.status(StatusCodes.ok).send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Welcome to Login-Lynx</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background-color: #f4f4f4;
-                margin: 0;
-                padding: 0;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-            }
-            .container {
-                background-color: white;
-                padding: 30px;
-                border-radius: 10px;
-                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-                text-align: center;
-                max-width: 400px;
-                width: 100%;
-            }
-            h1 {
-                color: #333;
-            }
-            p {
-                color: #555;
-            }
-            .btn {
-                background-color: #007BFF;
-                color: white;
-                padding: 10px 20px;
-                border: none;
-                border-radius: 5px;
-                cursor: pointer;
-                font-size: 16px;
-                text-decoration: none;
-                display: inline-block;
-                margin-top: 20px;
-            }
-            .btn:hover {
-                background-color: #0056b3;
-            }
-                img{
-                height:80px;
-                width:80px;
-                border-radius:50%;
-                }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <h1>Welcome to Login-Lynx</h1>
-            <p>Your one-stop solution for easy and secure login!</p>
-          <hr/>
-            ${user
-      ? `<div>
-                  <img src="${user?.avatar}"/>
-                <h3>You Are Live ${user.name}</h3>
-                   <a href="/api/v1/users" class="btn">View All users</a>
-                <div>`
-      : ` <a href="/api/v1/auth/google" class="btn">Continue With Google</a>`
-    }
-        </div>
-    </body>
-    </html>
-    `);
-});
 app.use("/api/v1", router);
 app.use(notFound);
 app.use(errorHandler);
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,5 +1,68 @@
+import mongoose from "mongoose";
+import z, { string, unknown } from "zod";
+
+/**
+ * Converts data to Array
+ * @type TData the type of data you want to receive, Default is any.
+ * @param data The data to convert.
+ */
 export function toArray<TData>(data: unknown) {
   return Array.isArray(data)
     ? data.map((item) => item as TData)
     : [data as TData];
+}
+
+
+/**
+ * Validates data against a Zod schema
+ * @param data - The data to be validated
+ * @param zodSchema - The Zod schema to validate against
+ * @returns An object indicating validation success, error message, and the validated data
+ */
+export function validateData<T>(
+  data: unknown,
+  zodSchema: z.ZodObject<any>
+): | { data: T, success: true } | { error: string, success: false } {
+  const validation = zodSchema.safeParse(data);
+
+  if (validation.success) {
+    return {
+      success: true,
+      data: validation.data as T,
+    };
+  } else {
+    return {
+      success: false,
+      error: validation.error.errors
+        .map((e) => `\n${e.path.join(".")}: ${e.message}`)
+        .join("\n-"),
+    };
+  }
+}
+
+
+
+
+
+/**
+ * Validates a Mongoose document before saving using the provided Zod schema.
+ * @param mongooseSchema The Mongoose schema.
+ * @param zodSchema The Zod schema for validation.
+ * @param name The name of the model (used for custom error handling).
+ */
+export function validateBeforeSave(
+  mongooseSchema: mongoose.Schema,
+  zodSchema: z.ZodObject<any>,
+  name: string
+) {
+  mongooseSchema.pre("save", function (this: mongoose.Document, next) {
+    const parsed = zodSchema.safeParse(this.toObject());
+    if (!parsed.success) {
+      const errorMessages = parsed.error.errors.map((e) => `${e.path}: ${e.message}`).join(", ");
+      const error = new Error(`Validation failed for ${name}: ${errorMessages}`);
+      next(error);
+    } else {
+      next();
+    }
+  });
 }
