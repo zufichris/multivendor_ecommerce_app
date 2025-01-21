8f4ac0419de6340c8d8fd41bed0d850c822c960a
zufichris <zufichris@gmail.com>
2025-01-21 23:04:12 +0400

    Updated logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,20 +1,34 @@
-import w, { Logform } from "winston"
+import w from "winston";
+
+w.addColors({
+    info: 'green',
+    error: 'red',
+    warn: 'yellow',
+    debug:"blue"
+});
+
+const customFormat = w.format.printf(({ timestamp, level, message }) => {
+    return `${level}: ${message}\t at:${timestamp}\n`;
+});
+
 export const logger = w.createLogger({
-    level: "info",
+    level: "debug",
     format: w.format.combine(
         w.format.timestamp(),
-        w.format.simple()
+        w.format.colorize(), 
+        customFormat 
     ),
     transports: [
         new w.transports.Console({
             format: w.format.combine(
                 w.format.colorize(),
-                w.format.simple()),
+                customFormat 
+            ),
         }),
         new w.transports.File({
             filename: 'logs/app.log',
             level: 'info',
+            format: customFormat,
         }),
-
     ],
-})
+});
