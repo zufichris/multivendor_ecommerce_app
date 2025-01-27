ff9a4e6674b423d4762b8ce354e7361e8c25e2f5
zufichris <zufichris@gmail.com>
2025-01-27 21:32:42 +0400

    Updated express types

--- src/global/express/index.d.ts
+++ src/global/express/index.d.ts
@@ -1,11 +1,9 @@
+import { TUser } from "../../data/entities/user";
 import { Role } from "../data/enums/user";
 import { ID } from "../entities";
 
 declare module 'express-serve-static-core' {
     interface Request {
-        user?: {
-            userId: ID;
-            roles: Role[];
-        }
+        user?: Partial<TUser>
     }
 }
