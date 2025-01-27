8efc08386ce5f60f3a496a0ecc2eed4ce8083479
zufichris <zufichris@gmail.com>
2025-01-27 21:34:53 +0400

    Updated Authcontrollers

--- src/http/controllers/auth/index.ts
+++ src/http/controllers/auth/index.ts
@@ -10,56 +10,68 @@ import { TUser } from "../../../data/entities/user";
 import { UserRepositoryImpl } from "../../../data/orm/repositoryImpl/user";
 import { UserModel } from "../../../data/orm/models/user";
 import { CreateUserUseCase } from "../../../domain/users/useCases/CreateUser";
-export class AuthControllers {
-  public googleAuthControllers = new GoogleAuthControllers(GoogleAuthConfig)
-  constructor(
-    private readonly authUseCase: AuthUseCase
-  ) { }
+import { IAuthUseCaseRepository } from "../../../domain/auth/repository";
 
-  async signUp(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
+export class AuthControllers {
+  public googleAuthControllers = new GoogleAuthControllers(GoogleAuthConfig);
+
+  constructor(private readonly authUseCase: IAuthUseCaseRepository) {
+    this.signUp = this.signUp.bind(this);
+    this.signIn = this.signIn.bind(this);
+    this.socialSignIn = this.socialSignIn.bind(this);
+    this.signOut = this.signOut.bind(this);
+    this.setCookies = this.setCookies.bind(this);
+    this.clearCookies = this.clearCookies.bind(this);
+  }
+
+  private generateMetadata(req: Request, message: string, type?: string) {
+    return {
+      url: req.url,
+      path: req.path,
+      type: type ?? "Auth",
+      message,
+      error: {
+        message,
+      },
+    };
+  }
+
+  async signUp(req: Request, res: Response, next: NextFunction) {
     try {
       const validation = validateData<TUser>(req.body, CreateUserSchema);
       if (!validation.success) {
-        res.status(EStatusCodes.enum.badRequest).json({
-          error: validation.error,
-          message: "Validation failed",
+        const data = {
+          ...this.generateMetadata(req, "Validation failed"),
           status: EStatusCodes.enum.badRequest,
+          success: false,
           description: "Invalid input data for creating a new user",
-          url: req.url,
-          path: req.path,
-        });
-        return
+          error: validation.error,
+        };
+        res.status(data.status).json(data);
+        return;
       }
 
       const result = await this.authUseCase.signUp(validation.data);
-
       if (!result.success) {
-        res.status(EStatusCodes.enum.conflict).json({
-          error: result.error,
-          message: "User creation conflict",
+        const data = {
+          ...this.generateMetadata(req, "User creation conflict"),
           status: EStatusCodes.enum.conflict,
+          success: false,
           description: "Email already in use or other conflict occurred",
-          url: req.url,
-          path: req.path,
-        });
-        return
+          error: result.error,
+        };
+        res.status(data.status).json(data);
+        return;
       }
-      const responseData: IResponseData<TUser> = {
-        ...result,
-        message: "New User Created",
-        status: EStatusCodes.enum.created,
-        description: "New user successfully created with email and password",
-        url: req.url,
-        path: req.path,
-        documentsModified: 1,
-        type: "Create User",
-      };
 
-      res.status(responseData.status).json(responseData);
+      const data: IResponseData<TUser> = {
+        ...this.generateMetadata(req, "New user created"),
+        status: EStatusCodes.enum.created,
+        success: true,
+        description: "New user successfully created with email and password",
+        data: result.data,
+      };
+      res.status(data.status).json(data);
     } catch (error) {
       next(error);
     }
@@ -68,106 +80,100 @@ export class AuthControllers {
     try {
       const validation = validateData<SignInDTO>(req.body, SignInSchema);
       if (!validation.success) {
-        res.status(EStatusCodes.enum.badRequest).json({
-          error: validation.error,
-          message: "Invalid login credentials",
+        const data = {
+          ...this.generateMetadata(req, "Invalid login credentials"),
           status: EStatusCodes.enum.badRequest,
+          success: false,
           description: "Validation failed for the provided email and password",
-          url: req.url,
-          path: req.path,
-        });
-        return
+          error: validation.error,
+        };
+        res.status(data.status).json(data);
+        return;
       }
+
       const { email, password } = validation.data;
       const result = await this.authUseCase.signIn({ email, password });
       if (!result.success) {
-        res.status(EStatusCodes.enum.unauthorized).json({
-          error: result.error,
-          message: "Authentication failed",
+        const data = {
+          ...this.generateMetadata(req, "Authentication failed"),
           status: EStatusCodes.enum.unauthorized,
+          success: false,
           description: "Invalid email or password",
-          url: req.url,
-          path: req.path,
-        });
-        return
+          error: result.error,
+        };
+        res.status(data.status).json(data);
+        return;
       }
-      const responseData = {
-        data: result.data,
-        message: "Login successful",
+
+      const data = {
+        ...this.generateMetadata(req, "Login successful"),
         status: EStatusCodes.enum.ok,
+        success: true,
         description: "User successfully logged in",
-        url: req.url,
-        path: req.path,
-        type: "Sign In",
+        data: result.data,
       };
 
       this.setCookies(
         res,
         result.data.tokenPair?.accessToken as string,
         result.data.tokenPair?.refreshToken as string
-      ).status(EStatusCodes.enum.ok).json(responseData);
+      ).status(data.status).json(data);
     } catch (error) {
       next(error);
     }
   }
-
 
   async socialSignIn(req: Request, res: Response, next: NextFunction) {
     try {
       const validation = validateData<SocialSignInDTO>(req.body, SocialSignInSchema);
       if (!validation.success) {
-        res.status(EStatusCodes.enum.badRequest).json({
-          error: validation.error,
-          message: "Invalid social login credentials",
+        const data = {
+          ...this.generateMetadata(req, "Invalid social login credentials"),
           status: EStatusCodes.enum.badRequest,
-          description: "Validation failed for the provided social login data",
-          url: req.url,
-          path: req.path,
-        });
-        return
+          success: false,
+          description: `Validation failed for  login data`,
+          error: validation.error,
+        };
+        res.status(data.status).json(data);
+        return;
       }
 
       const user = validation.data;
-
       const result = await this.authUseCase.signIn(user);
       if (!result.success) {
-        res.status(EStatusCodes.enum.unauthorized).json({
-          error: result.error,
-          message: "Social authentication failed",
+        const data = {
+          ...this.generateMetadata(req, "Social authentication failed"),
           status: EStatusCodes.enum.unauthorized,
+          success: false,
           description: "Social login failed for the provided user details",
-          url: req.url,
-          path: req.path,
-        });
-        return
+          error: result.error,
+        };
+        res.status(data.status).json(data);
+        return;
       }
-      const responseData = {
-        data: result.data,
-        message: "Social login successful",
-        status: EStatusCodes.enum.ok,
-        description: "User successfully logged in using social account",
-        url: req.url,
-        path: req.path,
-        type: "Social Sign In",
-      };
 
-      this.setCookies(
-        res,
-        result.data.tokenPair?.accessToken as string,
-        result.data.tokenPair?.refreshToken as string
-      ).status(EStatusCodes.enum.ok).json(responseData);
+      const data: IResponseData<TUser> = {
+        ...this.generateMetadata(req, "Social login successful"),
+        status: EStatusCodes.enum.ok,
+        success: true,
+        description: "User successfully logged in using social account",
+        data: result.data,
+      };
+      res.status(data.status).json(data);
     } catch (error) {
       next(error);
     }
   }
 
-
   async signOut(req: Request, res: Response, next: NextFunction) {
     try {
-      // const userId = req.user!.userId!;
-      // await this.authUseCase.signOut(userId);
-      this.clearCookies(res).status(EStatusCodes.enum.ok)
-        .json({ message: "Successfully logged out" });
+      this.clearCookies(res)
+        .status(EStatusCodes.enum.ok)
+        .json({
+          ...this.generateMetadata(req, "Successfully logged out"),
+          status: EStatusCodes.enum.ok,
+          success: true,
+        });
     } catch (error) {
       next(error);
     }
@@ -177,40 +183,41 @@ export class AuthControllers {
     return {
       accessTokenOptions: {
         httpOnly: true,
-        secure: true,
-        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
         maxAge: 15 * 60 * 1000,
+        domain: "localhost",
+        path: "/",
+        credentials: true,
       } as CookieOptions,
       refreshTokenOptions: {
         httpOnly: true,
-        secure: true,
-        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
+        domain: "localhost",
+        path: "/",
+        credentials: true,
       } as CookieOptions,
-    }
+    };
   }
 
-
-  private setCookies(
-    res: Response,
-    accessToken?: string,
-    refreshToken?: string
-  ) {
+  private setCookies(res: Response, accessToken?: string, refreshToken?: string) {
     const { accessTokenOptions, refreshTokenOptions } = this.getTokenOptions();
-
     res.cookie("access_token", accessToken, accessTokenOptions);
     res.cookie("refresh_token", refreshToken, refreshTokenOptions);
-    return res
+    return res;
   }
+
   private clearCookies(res: Response) {
     res.clearCookie("access_token");
     res.clearCookie("refresh_token");
-    return res
+    return res;
   }
 }
 
+const userRepository = new UserRepositoryImpl(UserModel);
+const createUser = new CreateUserUseCase(userRepository);
+const authUseCase = new AuthUseCase(userRepository, createUser);
 
-const userRepository = new UserRepositoryImpl(UserModel)
-const createUserUseCase = new CreateUserUseCase(userRepository)
-const authUseCase = new AuthUseCase(userRepository, createUserUseCase)
-export const authControllers = new AuthControllers(authUseCase)
+export const authControllers = new AuthControllers(authUseCase);
