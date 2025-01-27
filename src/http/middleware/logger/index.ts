6512d68f7e3c926b2870ea6618dc8b24be88378e
zufichris <zufichris@gmail.com>
2025-01-27 21:37:51 +0400

    Updated Logger middleware

--- src/http/middleware/logger/index.ts
+++ src/http/middleware/logger/index.ts
@@ -1,12 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 import { logger } from '../../../utils/logger';
 
-export const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const loggerMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const startTime = Date.now();
-    logger.info('Request received', { method: req.method, url: req.originalUrl });
+    const method = req.method
+    const path = req.path
+    logger.info(`Request received ${method}-${path}`, { method: req.method, url: req.originalUrl });
     res.on('finish', () => {
         const duration = Date.now() - startTime;
-        logger.info('Request processed', { method: req.method, url: req.originalUrl, status: res.statusCode, duration });
+        logger.info(`Request processed ${method}-${path} took: ${duration}ms`, { method: req.method, url: req.originalUrl, status: res.statusCode, duration });
     });
 
     next();
