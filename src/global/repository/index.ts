82ce5011950a381aa71c6363d982596d4395b651
zufichris <zufichris@gmail.com>
2025-01-27 21:33:23 +0400

    Updated global repositories

--- src/global/repository/index.ts
+++ src/global/repository/index.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import { ID, IQueryFilters, IQueryResult } from "../entities";
 
-export interface IBaseRepository<TData> {
+export interface IBaseRepository<TData, TQueryResult = IQueryResult<TData>> {
   create(data: Partial<TData>): Promise<TData | null>;
   update(id: ID, data: Partial<TData>): Promise<TData | null>;
   delete(id: ID): Promise<boolean>;
   findById(id: ID): Promise<TData | null>;
-  query(options?: IQueryFilters<TData>): Promise<IQueryResult<TData> | null>;
+  findOne(filter?: mongoose.RootFilterQuery<TData>): Promise<TData | null>;
+  query(options?: IQueryFilters<TData>): Promise<TQueryResult>;
   count(filter?: mongoose.FilterQuery<TData>): Promise<number | null>;
 }
