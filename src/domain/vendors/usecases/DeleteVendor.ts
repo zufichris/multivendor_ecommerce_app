757baf1fc6d61435c7c5120cb1e9435035a95132
zufichris <zufichris@gmail.com>
2025-02-04 19:20:13 +0400

    Added Delete Vendor usecase

---
+++ src/domain/vendors/usecases/DeleteVendor.ts
@@ -0,0 +1,25 @@
+import { EStatusCodes } from "../../../global/enums";
+import { AuthContext, BaseUseCase, handleUseCaseError, UseCaseResult } from "../../../global/useCase";
+import { IVendorRepository } from "../repository";
+
+export class DeleteVendorUseCase implements BaseUseCase<string, boolean, AuthContext> {
+    constructor(private readonly vendorRepository: IVendorRepository) { }
+
+    async execute(id: string, context: AuthContext): Promise<UseCaseResult<boolean>> {
+        try {
+            const existingVendor = await this.vendorRepository.findById(id);
+            if (!existingVendor) {
+                return handleUseCaseError({ error: "Vendor not found", title: "Delete Vendor", status: EStatusCodes.enum.notFound });
+            }
+
+            const deleted = await this.vendorRepository.delete(id);
+            if (!deleted) {
+                return handleUseCaseError({ error: "Error deleting vendor", title: "Delete Vendor" });
+            }
+
+            return { data: true, success: true };
+        } catch (error) {
+            return handleUseCaseError({ title: "Delete Vendor", status: 500 });
+        }
+    }
+}
