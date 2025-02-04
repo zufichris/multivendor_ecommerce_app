360c9fa099d7b6bf85e6e4ed88500f10d0a20888
zufichris <zufichris@gmail.com>
2025-02-04 19:24:49 +0400

    Query vendors

---
+++ src/domain/vendors/usecases/QueryVendors.ts
@@ -0,0 +1,26 @@
+import { TVendor } from "../../../data/entities/vendor";
+import { IQueryFilters, IQueryResult } from "../../../global/entities";
+import { EStatusCodes } from "../../../global/enums";
+import { AuthContext, BaseUseCase, handleUseCaseError, UseCaseResult } from "../../../global/useCase";
+import { isAdmin } from "../../../utils/functions";
+import { IVendorRepository } from "../repository";
+
+export class GetVendorsUseCase implements BaseUseCase<IQueryFilters<TVendor>, IQueryResult<TVendor>, AuthContext> {
+    constructor(private readonly vendorRepository: IVendorRepository) { }
+
+    async execute(options?: IQueryFilters<TVendor>, context?: AuthContext): Promise<UseCaseResult<IQueryResult<TVendor>>> {
+        try {
+            if (!isAdmin(context?.roles)) {
+                return handleUseCaseError({ title: "Forbidden", status: EStatusCodes.enum.forbidden })
+            }
+            const result = await this.vendorRepository.query(options);
+            if (!result) {
+                return handleUseCaseError({ error: "Error fetching vendors", title: "Get Vendors" });
+            }
+
+            return { data: result, success: true };
+        } catch (error) {
+            return handleUseCaseError({ title: "Get Vendors", status: 500 });
+        }
+    }
+}
