a6336df6d015d0e7a8a205d1d366ca5849f25357
zufichris <zufichris@gmail.com>
2025-02-05 17:50:36 +0400

    Added Usecase Product Update

---
+++ src/domain/products/useCases/UpdateProduct.ts
@@ -0,0 +1,30 @@
+import { TProduct } from "../../../data/entities/product";
+import { EStatusCodes } from "../../../global/enums";
+import { AuthContext, BaseUseCase, handleUseCaseError, UseCaseResult } from "../../../global/useCase";
+import { isVendor } from "../../../utils/functions";
+import { IProductRepository } from "../repository";
+
+export class UpdateProductUseCase implements BaseUseCase<{ id: string; data: Partial<TProduct> }, TProduct | null, AuthContext> {
+    constructor(private readonly productRepository: IProductRepository) { }
+
+    async execute(input: { id: string; data: Partial<TProduct> }, context?: AuthContext): Promise<UseCaseResult<TProduct | null>> {
+        try {
+            if (!isVendor(context?.roles)) {
+                return handleUseCaseError({ error: "Unauthorized", title: "Update Product", status: EStatusCodes.enum.forbidden });
+            }
+
+            const { id, data } = input;
+            const updatedProduct = await this.productRepository.update(id, data);
+            if (!updatedProduct) {
+                return handleUseCaseError({ error: "Product Not Found", title: "Update Product", status: EStatusCodes.enum.notFound });
+            }
+
+            return {
+                success: true,
+                data: updatedProduct,
+            };
+        } catch (error) {
+            return handleUseCaseError({ title: "Update Product" });
+        }
+    }
+}
