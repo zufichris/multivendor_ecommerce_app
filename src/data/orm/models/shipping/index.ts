bd5ba6a40812142d18eea3ebb72cd8de90bc5baf
zufichris <zufichris@gmail.com>
2025-02-07 16:17:41 +0400

    Added Models for Shipping

---
+++ src/data/orm/models/shipping/index.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import { EShippingStatus, ShippingSchema, TShipping } from "../../../entities/shipping";
+import { validateBeforeSave } from "../../../../utils/functions";
+
+export type ShippingDocument = TShipping & mongoose.Document
+
+const ShippingStatusHistorySchema = new mongoose.Schema(
+    {
+        status: {
+            type: String,
+            enum: Object.values(EShippingStatus.enum),
+            required: true,
+        },
+        changedAt: { type: Date, required: true },
+        reason: { type: String },
+    },
+    { _id: false }
+);
+
+const schema = new mongoose.Schema<ShippingDocument>({
+    addressId: { type: String, ref: "Address" },
+    userId: { type: String, required: true },
+    trackingNumber: { type: String },
+    carrier: { type: String },
+    status: {
+        type: String,
+        enum: Object.values(EShippingStatus.enum),
+        default: 'PENDING',
+    },
+    estimatedDelivery: { type: Date },
+    statusHistory: { type: [ShippingStatusHistorySchema], default: [] },
+}, {
+    timestamps: true,
+    toJSON: {
+        transform: (doc) => {
+            delete doc?._id
+        },
+        versionKey: false,
+        virtuals: true
+    }
+});
+
+validateBeforeSave(schema, ShippingSchema, "Shipping")
+
+export const ShippingModel: mongoose.Model<ShippingDocument> = mongoose.models.Shipping || mongoose.model("Shipping", schema)
