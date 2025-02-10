2b8709c6713960009f5cdaedc77d504adad4c4b4
zufichris <zufichris@gmail.com>
2025-02-10 20:00:25 +0400

    Add entitties for payment-method module

--- src/data/entity/payment-method/index.ts
+++ src/data/entity/payment-method/index.ts
@@ -4,8 +4,9 @@ import { ECurrency } from "../../../global/enum";
 export const PaymentMethodSchema = z.object({
     id: z.string().optional(),
     name: z.string(),
+    pmtId: z.string().optional(),
     description: z.string(),
-    logo: z.string(),
+    logo: z.string().optional(),
     supportedCurrencies: z.array(z.nativeEnum(ECurrency.Enum)).default(Object.values(ECurrency.enum)),
     feeStructure: z
         .object({
@@ -20,15 +21,6 @@ export const PaymentMethodSchema = z.object({
     updatedAt: z.string().datetime(),
 })
     .strict()
-    .refine((data) => {
-        if (data.maxAmount) {
-            return data.maxAmount >= data.minAmount;
-        }
-        return true;
-    }, {
-        message: "maxAmount must be greater than or equal to minAmount",
-        path: ["maxAmount"],
-    })
 
 export type TPaymentMethod = z.infer<typeof PaymentMethodSchema>
 
