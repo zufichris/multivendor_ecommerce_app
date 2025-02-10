4e1f2eb06e31ddcf1f6a0ed2c29c1d8b3be804aa
zufichris <zufichris@gmail.com>
2025-02-10 19:54:09 +0400

    Added create-payment.ts usecase

---
+++ src/domain/payment/use-case/create-payment.ts
@@ -0,0 +1,56 @@
+import { TPayment } from "../../../data/entity/payment";
+import { AuthContext, BaseUseCase, handleUseCaseError, UseCaseResult } from "../../../global/use-case";
+import { validateData } from "../../../util/functions";
+import { EStatusCodes } from "../../../global/enum";
+import { CreatePaymentDTO, CreatePaymentSchema } from "../../../data/dto/payment";
+import { IPaymentRepository } from "../repository";
+
+export class CreatePaymentUseCase implements BaseUseCase<CreatePaymentDTO, TPayment, AuthContext> {
+    constructor(private readonly paymentRepository: IPaymentRepository) { }
+
+    async execute(input: CreatePaymentDTO, context: AuthContext): Promise<UseCaseResult<TPayment>> {
+        try {
+            if (!context.userId) {
+                return handleUseCaseError({
+                    title: "Forbidden",
+                    error: "User authentication is required to create a payment.",
+                    status: EStatusCodes.enum.forbidden
+                });
+            }
+
+            const validation = validateData<CreatePaymentDTO>(input, CreatePaymentSchema);
+            if (!validation.success) {
+                return handleUseCaseError({
+                    title: "Create Payment",
+                    error: validation.error,
+                    status: EStatusCodes.enum.badRequest
+                });
+            }
+
+            const data: Partial<TPayment> = {
+                ...validation.data,
+                status: "PENDING",
+            };
+
+            const createdPayment = await this.paymentRepository.create(data);
+            if (!createdPayment) {
+                return handleUseCaseError({
+                    title: "Create Payment",
+                    error: "An error occurred while processing the payment.",
+                    status: EStatusCodes.enum.internalServerError
+                });
+            }
+
+            return {
+                data: createdPayment,
+                success: true,
+            };
+        } catch (error) {
+            return handleUseCaseError({
+                title: "Create Payment",
+                error: "Unexpected error occurred during payment creation.",
+                status: 500
+            });
+        }
+    }
+}
