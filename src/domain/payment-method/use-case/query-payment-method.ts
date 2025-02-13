358783cf052aa2646b98308f77c86f555180220b
zufichris <zufichris@gmail.com>
2025-02-13 18:42:22 +0400

    added permissions for ./query-payment-method.ts

--- src/domain/payment-method/use-case/query-payment-method.ts
+++ src/domain/payment-method/use-case/query-payment-method.ts
@@ -1,8 +1,6 @@
 import { TPaymentMethod } from "../../../data/entity/payment-method";
 import { IQueryFilters, IQueryResult } from "../../../global/entity";
-import { EStatusCodes } from "../../../global/enum";
 import { AuthContext, BaseUseCase, handleUseCaseError, UseCaseResult } from "../../../global/use-case";
-import { isAdmin } from "../../../util/functions";
 import { IPaymentMethodRepository } from "../repository";
 
 export class QueryPaymentMethodUseCase implements BaseUseCase<IQueryFilters<TPaymentMethod>, IQueryResult<TPaymentMethod>, AuthContext> {
@@ -10,9 +8,6 @@ export class QueryPaymentMethodUseCase implements BaseUseCase<IQueryFilters<TPay
 
     async execute(options?: IQueryFilters<TPaymentMethod>, context?: AuthContext): Promise<UseCaseResult<IQueryResult<TPaymentMethod>>> {
         try {
-            if (!isAdmin(context?.roles)) {
-                return handleUseCaseError({ title: "Forbidden", status: EStatusCodes.enum.forbidden });
-            }
             const result = await this.paymentMethodRepository.query(options);
             if (!result) {
                 return handleUseCaseError({ error: "Error fetching payment methods", title: "Query Payment Method" });
