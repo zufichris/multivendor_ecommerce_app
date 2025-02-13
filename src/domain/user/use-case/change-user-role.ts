c05262dde3cbd66048bc0b6d15f7267d1a7600c6
zufichris <zufichris@gmail.com>
2025-02-13 18:42:37 +0400

    added permissions for ./change-user-role.ts

--- src/domain/user/use-case/change-user-role.ts
+++ src/domain/user/use-case/change-user-role.ts
@@ -3,16 +3,25 @@ import { AuthContext, BaseUseCase, handleUseCaseError, UseCaseResult } from "../
 import { IUserRepository } from "../repository";
 import { EStatusCodes } from "../../../global/enum";
 import { Role } from "../../../data/enum/user";
+import { getPermission, hasRequiredPermissions } from "../../../util/functions";
 
 export class ChangeUserRoleUseCase implements BaseUseCase<{ userId: string, roles: Role[] }, TUser, AuthContext> {
     constructor(private readonly userRepository: IUserRepository) { }
 
     async execute(input: { userId: string, roles: Role[] }, context: AuthContext): Promise<UseCaseResult<TUser>> {
         try {
-            if (!context?.userId || (context?.roles.includes(Role.Admin))) {
+            if (!context?.userId) {
                 return handleUseCaseError({ error: "Unauthorized", title: "Change User Role", status: EStatusCodes.enum.forbidden });
             }
-
+            const REQUIRED_PERMISSION = getPermission("user", "manage");
+            const hasPermission = hasRequiredPermissions(REQUIRED_PERMISSION, context.permissions);
+            if (!hasPermission) {
+                return handleUseCaseError({
+                    error: "Forbidden: You do not have permission to change user roles.",
+                    title: "Change Role - Authorization",
+                    status: EStatusCodes.enum.forbidden,
+                });
+            }
             const data = await this.userRepository.update(input.userId, {
                 roles: input.roles
             });
