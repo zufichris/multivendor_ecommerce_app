f32e8a0f26e3fd968fd331613fddd20f89a13a2c
zufichris <zufichris@gmail.com>
2025-02-13 18:46:50 +0400

    added permissions for ./shipping routes

--- src/http/routes/shipping/index.ts
+++ src/http/routes/shipping/index.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { shippingControllers } from '../../controller/shipping';
+import { authMiddleware } from '../../middleware/auth';
 
 const router = Router();
-
+router.use(authMiddleware.requireAuth,authMiddleware.requirePermission("shipping","manage_own"))
 router.route("/")
     .post(shippingControllers.createShipping)
     .get(shippingControllers.queryShippings);
