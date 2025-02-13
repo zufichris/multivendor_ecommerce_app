d9cd00bca54b603eb75a4140006c02ebdfadc959
zufichris <zufichris@gmail.com>
2025-02-13 18:46:48 +0400

    added permissions for ./order routes

--- src/http/routes/order/index.ts
+++ src/http/routes/order/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { orderControllers } from '../../controller/order';
+import { authMiddleware } from '../../middleware/auth';
 
 const router = Router();
-
+router.use(authMiddleware.requireAuth, authMiddleware.requirePermission("order", "manage_own"),)
 router.route("/")
     .post(orderControllers.createOrder)
     .get(orderControllers.queryOrders);
-router.route("/:orderId")
+router.use(authMiddleware.requirePermission("order", "manage_own")).route("/:orderId")
     .get(orderControllers.getOrder)
     .put(orderControllers.updateOrder)
     .post(orderControllers.cancelOrder);
