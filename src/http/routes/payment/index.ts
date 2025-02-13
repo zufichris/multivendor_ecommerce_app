5c695fe816e6aeef855efa3f1952643bc047e716
zufichris <zufichris@gmail.com>
2025-02-13 18:46:48 +0400

    added permissions for ./payment routes

--- src/http/routes/payment/index.ts
+++ src/http/routes/payment/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { paymentControllers } from '../../controller/payment';
+import { authMiddleware } from '../../middleware/auth';
 
 const router = Router();
 
 router.route("/")
     .post(paymentControllers.createPayment)
-    .get(paymentControllers.queryPayments);
-router.route("/:paymentId")
+    .get(authMiddleware.requirePermission("vendor", "manage"), paymentControllers.queryPayments);
+router.use(authMiddleware.requirePermission("vendor", "manage_own"),).route("/:paymentId")
     .get(paymentControllers.getPayment)
     .put(paymentControllers.updatePayment)
     .delete(paymentControllers.deletePayment);
