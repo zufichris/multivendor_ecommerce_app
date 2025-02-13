27b7e316ec5aa506ffab5b5baf85e15e05295c82
zufichris <zufichris@gmail.com>
2025-02-13 18:46:49 +0400

    added permissions for ./product routes

--- src/http/routes/product/index.ts
+++ src/http/routes/product/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import { productControllers } from "../../controller/product"
+import { authMiddleware } from "../../middleware/auth"
 const router = express.Router()
 router.route("/")
     .get(productControllers.queryProducts)
@@ -9,7 +10,7 @@ router.route("/:slug")
     .get(productControllers.getProduct)
 
 router.route("/:id")
-    .patch(productControllers.updateProductStatus)
-    .delete(productControllers.deleteProduct)
+    .patch(authMiddleware.requireAuth, authMiddleware.requirePermission("product", "manage_own"), productControllers.updateProductStatus)
+    .delete(authMiddleware.requireAuth, authMiddleware.requirePermission("product", "manage_own"), productControllers.deleteProduct)
 
 export default router
