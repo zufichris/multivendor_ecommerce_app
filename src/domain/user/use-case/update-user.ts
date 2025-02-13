ffee8647959c4a882801a072d6dc0e070d09d3f0
zufichris <zufichris@gmail.com>
2025-02-13 18:42:40 +0400

    added permissions for ./update-user.ts

--- src/domain/user/use-case/update-user.ts
+++ src/domain/user/use-case/update-user.ts
@@ -2,17 +2,25 @@ import { TUser } from "../../../data/entity/user";
 import { UpdateUserDTO, UpdateUserSchema } from "../../../data/dto/user";
 import { AuthContext, BaseUseCase, handleUseCaseError, UseCaseResult } from "../../../global/use-case";
 import { IUserRepository } from "../repository";
-import { validateData } from "../../../util/functions";
+import { getPermission, hasRequiredPermissions, validateData } from "../../../util/functions";
 import { EStatusCodes } from "../../../global/enum";
-import { Role } from "../../../data/enum/user";
 
 export class UpdateUserUseCase implements BaseUseCase<UpdateUserDTO, TUser, AuthContext> {
     constructor(private readonly userRepository: IUserRepository) { }
 
     async execute(input: UpdateUserDTO, context: AuthContext): Promise<UseCaseResult<TUser>> {
         try {
-            if (!context?.userId || (context?.roles.includes(Role.Admin))) {
+            if (!context?.userId) {
                 return handleUseCaseError({ error: "Unauthorized", title: "Update User", status: EStatusCodes.enum.forbidden })
+            }
+            const REQUIRED_PERMISSION = getPermission("user", "manage_own");
+            const hasPermission = hasRequiredPermissions(REQUIRED_PERMISSION, context.permissions);
+            if (!hasPermission) {
+                return handleUseCaseError({
+                    error: "Forbidden: You do not have permission to create roles.",
+                    title: "Create Role - Authorization",
+                    status: EStatusCodes.enum.forbidden,
+                });
             }
             const validate = validateData<UpdateUserDTO>(input, UpdateUserSchema);
             if (!validate.success) {
