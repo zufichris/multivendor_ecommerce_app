eb3d21f08cb9a2c7e54793b08f84e5548c27d34e
zufichris <zufichris@gmail.com>
2025-02-13 18:46:51 +0400

    added permissions for ./vendor routes

--- src/http/routes/vendor/index.ts
+++ src/http/routes/vendor/index.ts
@@ -1,21 +1,21 @@
 import express from "express"
-import { authMiddleWare } from "../../middleware/auth"
+import { authMiddleware } from "../../middleware/auth"
 import { Role } from "../../../data/enum/user"
 import { vendorControllers } from "../../controller/vendor"
 const router = express.Router()
 
-router.use(authMiddleWare.requireAuth).use((req, res, next) => authMiddleWare.authorize([Role.Vendor, Role.Admin], req, res, next))
+router.use(authMiddleware.requireAuth)
 
 router.route("/")
-    .get(vendorControllers.queryVendors)
+    .get(authMiddleware.requirePermission("vendor", "manage"), vendorControllers.queryVendors)
     .post(vendorControllers.createVendor)
 
-router.route('/:vendId')
+router.use(authMiddleware.requirePermission("vendor", "manage_own"),).route('/:vendId')
     .get(vendorControllers.getVendor)
     .patch(vendorControllers.updateVendor)
     .delete(vendorControllers.deleteVendor)
 
-router.route("/:vendId/verify",)
+router.use(authMiddleware.requirePermission("vendor", "manage"),).route("/:vendId/verify")
     .patch(vendorControllers.verifyVendor)
 
 export default router
