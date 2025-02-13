b7d451b277e193485b403a45f340f1bb392f0dd9
zufichris <zufichris@gmail.com>
2025-02-13 18:46:49 +0400

    added permissions for ./payment-method routes

--- src/http/routes/payment-method/index.ts
+++ src/http/routes/payment-method/index.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import { paymentMethodControllers } from '../../controller/payment-method';
+import { authMiddleware } from '../../middleware/auth';
 
 const router = Router();
-
 router.route("/")
-    .post(paymentMethodControllers.createPaymentMethod)
+    .post(authMiddleware.requireAuth, authMiddleware.requirePermission("payment-method", "manage"), paymentMethodControllers.createPaymentMethod)
     .get(paymentMethodControllers.queryPaymentMethods);
 
 router.route("/:paymentId")
     .get(paymentMethodControllers.getPaymentMethod)
-    .put(paymentMethodControllers.updatePaymentMethod)
-    .delete(paymentMethodControllers.deletePaymentMethod);
+    .put(authMiddleware.requireAuth, authMiddleware.requirePermission("payment-method", "manage"), paymentMethodControllers.updatePaymentMethod)
+    .delete(authMiddleware.requirePermission("payment-method", "manage"),paymentMethodControllers.deletePaymentMethod);
 
 export default router;
