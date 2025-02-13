6df85ec1326bdb211bac53b1c26417be333a78db
zufichris <zufichris@gmail.com>
2025-02-13 18:42:38 +0400

    added permissions for ./change-user-status.ts

--- src/domain/user/use-case/change-user-status.ts
+++ src/domain/user/use-case/change-user-status.ts
@@ -2,18 +2,26 @@ import { TUser } from "../../../data/entity/user";
 import { AuthContext, BaseUseCase, handleUseCaseError, UseCaseResult } from "../../../global/use-case";
 import { IUserRepository } from "../repository";
 import { EStatusCodes } from "../../../global/enum";
-import { Role } from "../../../data/enum/user";
 import { ID } from "../../../global/entity";
+import { getPermission, hasRequiredPermissions } from "../../../util/functions";
 
 export class ChangeUserStatusUseCase implements BaseUseCase<{ userId: string, isActive: boolean }, TUser, AuthContext> {
     constructor(private readonly userRepository: IUserRepository) { }
 
     async execute(input: { userId: ID, isActive: boolean }, context: AuthContext): Promise<UseCaseResult<TUser>> {
         try {
-            if (!context?.userId || (context?.roles.includes(Role.Admin))) {
+            if (!context?.userId) {
                 return handleUseCaseError({ error: "Unauthorized", title: "Change User Status", status: EStatusCodes.enum.forbidden });
             }
-
+            const REQUIRED_PERMISSION = getPermission("user", "manage");
+            const hasPermission = hasRequiredPermissions(REQUIRED_PERMISSION, context.permissions);
+            if (!hasPermission) {
+                return handleUseCaseError({
+                    error: "Forbidden: You do not have permission to change user status.",
+                    title: "Change Status - Authorization",
+                    status: EStatusCodes.enum.forbidden,
+                });
+            }
             const data = await this.userRepository.update(input.userId, {
                 isActive: input.isActive
             });
